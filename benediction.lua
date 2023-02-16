acf48ca696a2c8004b81ac4c9bb5b6c16dc82327
Andrew Krufka <A.Krufka@aol.com>
2023-02-16 18:19:06 +0000

    dd

--- benediction.lua
+++ benediction.lua
@@ -1,3 +1,4 @@
 local addonName, addonTable = ...
 
 addonTable.benediction_blacklist = {'Souline', 'Remour', 'Doomwalkers', 'Moongladis', 'Moonvenger'}
+addonTable.benediction_black_date = "Benediction Blacklist Last Updated On: 2023-02-16"
